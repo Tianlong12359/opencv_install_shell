b8d20998d215c4a535cc95568a6915485cea8b6f
Tianlong12359 <37934537+Tianlong12359@users.noreply.github.com>
2019-10-05 13:49:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 
-cd /home/
+cd /home/                                                                                                                                                             #改变安装目录
 
 sudo apt install git cmake
 
-git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv.git                                                                               #下载主文件
 
-cd opencv&&mkdir release&&cd release
+cd opencv&&mkdir release&&cd release                                                                                               #转移至安装目录
 
-cmake -D CMAKE_BUILD_TYPE=Release  -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make
+cmake -D CMAKE_BUILD_TYPE=Release  -D CMAKE_INSTALL_PREFIX=/usr/local ..           #开始设置
+
+
+make                                                                                                                                                                      #开始编译安装                                                                                                                                             
 sudo make install
